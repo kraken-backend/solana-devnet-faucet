305df79335e1143bf838961764deae5308671a7a
ferric <ferric-sol@ferric.net>
2025-02-20 01:56:10 -0700

    Fix bugs

---
+++ auth/config.ts
@@ -0,0 +1,19 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+}; 
