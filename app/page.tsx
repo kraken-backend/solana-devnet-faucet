1a5706458d955dc0db3f845a382bf587ca0b97c0
ferric <ferric-sol@ferric.net>
2025-02-20 01:53:33 -0700

    Fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { FaucetForm } from "./components/FaucetForm";
 import { getServerSession } from "next-auth/next";
 import { SignInButton } from "./components/SignInButton";
-import { authOptions } from '@/app/utils/auth';
+import { authOptions } from './utils/auth';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
